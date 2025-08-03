19fb0563ebde8e02814b078d16b9758d7cc53139
TDMario <forrermario@outlook.com>
2025-08-03 12:06:52 +0200

    fix create_assistant.py

--- create_assistant.py
+++ create_assistant.py
@@ -1,11 +1,13 @@
 from openai import OpenAI
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 assistant = client.beta.assistants.create(
     name="Doc Assistant",
-    instructions="You are a helpful assistant. Answer questions based on the provided documents.",
+    instructions="You are a helpful assistant. Answer questions based on the provided documents and only the provided documents. If you cant find it in there, you dont know the answer",
     model="gpt-4-1106-preview",  # or gpt-4-turbo if you want streaming
     tools=[{"type": "file_search"}]
 )
